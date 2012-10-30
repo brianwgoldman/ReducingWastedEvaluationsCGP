5335041de225fc60a7a985c52160867132b92220
Goldman <brianwgoldman@acm.org>
2012-10-30 16:30:11 -0400

    Removed unnecessary files from release branch

--- runone.sh
+++
@@ -1,9 +0,0 @@
-PROBLEM=$1
-NODES=$2
-MUT=$3
-SEED=$4
-SUB=$5
-
-sed -e "s/PROBLEM/$PROBLEM/g" -e "s/NODES/$NODES/g" -e "s/MUT/$MUT/g" -e "s/SEED/$SEED/g" $SUB > temp.sub
-qsub temp.sub
-rm temp.sub
